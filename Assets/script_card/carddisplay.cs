f607efb123031d2fa298c0d9d78993f89ea0db8a
siki <2878086171@qq.com>
2025-05-30 17:41:42 +0800

    card_1

---
+++ Assets/script_card/carddisplay.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+public class carddisplay : MonoBehaviour
+{
+    
+    
+    public Text Text_1;
+    public Text Text_2;
+    public Text Text_3;
+    public Text Text_4;
+
+    public Image Image_1;
+    public Image Image_2;
+    public Image Image_3;
+    public Image Image_4;
+    public Image Image_5;
+    public Image Image_6;
+    public Image Image_7;
+
+    public CardMessage Card_1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //Text_1.text = Card_1.Card_Name;
+        //Text_2.text = Card_1.Card_Text;
+        //Text_3.text = Card_1.Card_Cost.ToString();
+        //Text_4.text = Card_1.Card_Level.ToString();     
+
+        //Image_1.sprite = Card_1.Card_Art;
+        //Image_2.sprite = Card_1.Card_NameBack;
+        //Image_3.sprite = Card_1.Card_LevelBack;
+        //Image_4.sprite = Card_1.Card_CostBack;
+        //Image_5.sprite = Card_1.Card_TextBack;
+        //Image_6.sprite = Card_1.Card_Back;
+        //Image_7.sprite = Card_1.Card_Frame;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+
+
+
+
+}
